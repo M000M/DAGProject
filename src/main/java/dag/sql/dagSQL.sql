42d944aed21ae8c4eeecf672b5a2116e3162e0d3
tancong <itancong@163.com>
2021-04-26 22:35:22 +0800

    04.26 windows

--- src/main/java/dag/sql/dagSQL.sql
+++ src/main/java/dag/sql/dagSQL.sql
@@ -1,12 +1,14 @@
-CREATE DATABASE dag;
+CREATE DATABASE IF NOT EXISTS `dag`;
 
-USE dag;
+USE `dag`;
+
+DROP TABLE IF EXISTS `dag_block`;
 
 CREATE TABLE dag_block(
     `id`   INT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `pre1` VARCHAR(100),
     `pre2` VARCHAR(100),
-    `data` VARCHAR(100),
+    `data` TEXT,
     `hash` VARCHAR(100),
     `num`  INT(20) NOT NULL DEFAULT 0,
     `timestamp` bigint(20) NOT NULL
